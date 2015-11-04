bc188a1b7ac46ef30f0b761191137070a75fdd61
Adam Ferras <aferras@articulate.com>
2015-11-04 09:30:44 -0700

    Error handling in each environment

--- test/app_test.js
+++ test/app_test.js
@@ -4,8 +4,33 @@ var server = require('../app');
 var should = chai.should();
 var expect = require('expect.js');
 var nock = require('nock');
+var assert = chai.assert;
 
 chai.use(chaiHttp);
+
+function stack_trace_404(env) {
+  process.env.NODE_ENV = env;
+  it('should 404 if navigating to a page that does not exist', function(done) {
+    chai.request(server)
+    .get('/doesnotexist')
+    .end(function(err, res){
+      expect(res.status).to.equal(404);
+      expect(res.text).to.contain('Not Found');
+      if (process.env.NODE_ENV === 'production') {
+        expect(res.text).to.not.contain('Error');
+      } else {
+        expect(res.text).to.contain('Error');
+      }
+      done();
+    });
+  });
+};
+
+describe('test stack trace visibility in each environment', function() {
+  stack_trace_404('development');
+  stack_trace_404('test');
+  stack_trace_404('production');
+});
 
 describe('App', function() {
   it('should show the home page', function(done) {
@@ -18,13 +43,6 @@ describe('App', function() {
     });
   });
 
-  it('should 404 if navigating to a page that does not exist', function(done) {
-    chai.request(server)
-    .get('/doesnotexist')
-    .end(function(err, res){
-      expect(res.status).to.equal(404);
-      expect(res.text).to.contain('Not Found');
-      done();
-    });
+  xit('should generate a 500 error', function(done) {
   });
 });
